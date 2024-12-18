bbf945ef27cf479011edbf35ad5750ec9bc141d3
Git hub User Sanjay <github_cloudlabsuser_1270@cloudlabsaiuser.com>
2024-12-18 08:14:08 +0000

    updated the deploy json files

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed3381d6cf59bdae5f00dce384139741aa1dd77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d5f7db52781d7c01e06ed72a5e5368c6a0be03e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
